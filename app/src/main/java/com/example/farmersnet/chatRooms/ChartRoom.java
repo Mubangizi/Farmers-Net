f0290bacf8ce0e59a0c4cd13767ec4f84d32163e
Mubangizi <mubangizia22@gmail.com>
2019-09-05 06:57:04 -0700

    create chatroom page

--- app/src/main/java/com/example/farmersnet/chatRooms/ChartRoom.java
+++
@@ -1,4 +0,0 @@
-package com.example.farmersnet.chatRooms;
-
-public class ChartRoom {
-}
---
+++ app/src/main/java/com/example/farmersnet/chatRooms/CreateChatRoomActivity.java
@@ -0,0 +1,13 @@
+package com.example.farmersnet.chatRooms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateChatRoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_chat_room);
+    }
+}
