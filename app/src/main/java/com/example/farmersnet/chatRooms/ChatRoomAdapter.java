8971159b841007b31beae0bdddaa2ef061f2b6d1
Mubangizi <mubangizia22@gmail.com>
2019-09-06 04:09:10 -0700

    creating chatpage

--- app/src/main/java/com/example/farmersnet/chatRooms/ChatRoomAdapter.java
+++ app/src/main/java/com/example/farmersnet/chatRooms/ChatRoomAdapter.java
@@ -1,6 +1,7 @@
 package com.example.farmersnet.chatRooms;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -9,8 +10,10 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.farmersnet.ChatActivity;
 import com.example.farmersnet.R;
 import com.example.farmersnet.post.Post;
+import com.example.farmersnet.post.PostActivity;
 import com.example.farmersnet.utils.FirebaseUtil;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -32,13 +35,26 @@ public class ChatRoomAdapter extends RecyclerView.Adapter<ChatRoomAdapter.ChatRo
     public ChatRoomViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         context = viewGroup.getContext();
         View view = LayoutInflater.from(context).inflate(R.layout.chartroom_layout, viewGroup,false);
+
         return new ChatRoomViewHolder(view);
     }
+
+    private void sendToPost() {
+        Intent messageIntent = new Intent(context, ChatActivity.class);
+        context.startActivity(messageIntent);
+    }
+
 
     @Override
     public void onBindViewHolder(@NonNull ChatRoomViewHolder chatRoomViewHolder, int i) {
         final ChatRoom chatRoom = chatRoomArrayList.get(i);
         chatRoomViewHolder.bind(chatRoom);
+        chatRoomViewHolder.nameTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendToPost();
+            }
+        });
     }
 
     @Override
@@ -47,7 +63,7 @@ public class ChatRoomAdapter extends RecyclerView.Adapter<ChatRoomAdapter.ChatRo
     }
 
     public class ChatRoomViewHolder extends RecyclerView.ViewHolder{
-        private TextView nameTextView;
+        public  TextView nameTextView;
         private TextView descTextVew;
         private ImageView roomImage;
         public ChatRoomViewHolder(@NonNull View itemView) {
--- app/src/main/java/com/example/farmersnet/utils/UploadImageUtil.java
+++ app/src/main/java/com/example/farmersnet/utils/UploadImageUtil.java
@@ -1,4 +1,37 @@
 package com.example.farmersnet.utils;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+import com.example.farmersnet.RegisterActivity;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
 public class UploadImageUtil {
+
+    private static Activity activity;
+    public void checkPermission(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            if (ContextCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                //ask for permission
+                ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            }else {
+                startCropActivity();
+            }
+        }else {
+            startCropActivity();
+        }
+    }
+
+    private static void startCropActivity() {
+        CropImage.activity()
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .start(activity);
+    }
+
 }
