9f74394c61d66c3aaab384094bf058cb82bc0d1f
Mubangizi <mubangizia22@gmail.com>
2019-09-18 04:06:33 +0000

    set chat adapter

--- app/src/main/java/com/example/farmersnet/messages/ChatActivity.java
+++ app/src/main/java/com/example/farmersnet/messages/ChatActivity.java
@@ -1,15 +1,32 @@
 package com.example.farmersnet.messages;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 
 import com.example.farmersnet.R;
 
+import java.util.ArrayList;
+
+import static android.app.PendingIntent.getActivity;
+
 public class ChatActivity extends AppCompatActivity {
+
+    private RecyclerView chatRecyclerView;
+    private ChatRecyclerAdapter chatRecyclerAdapter;
+    private ArrayList<Message> messageArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        messageArrayList = new ArrayList<Message>();
+        chatRecyclerAdapter = new ChatRecyclerAdapter(messageArrayList);
+        chatRecyclerView.setLayoutManager(new LinearLayoutManager(ChatActivity.this, LinearLayoutManager.VERTICAL, false));
+        chatRecyclerView.setAdapter(chatRecyclerAdapter);
+
     }
 }
