48fa3fac0586f466320d201de33b0c2f20d531f1
Mubangizi <mubangizia22@gmail.com>
2019-09-23 08:46:09 +0000

    able to retrieve comments

--- app/src/main/java/com/example/farmersnet/post/PostActivity.java
+++ app/src/main/java/com/example/farmersnet/post/PostActivity.java
@@ -1,26 +1,41 @@
 package com.example.farmersnet.post;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.farmersnet.R;
+import com.example.farmersnet.messages.ChatActivity;
+import com.example.farmersnet.messages.Message;
 import com.example.farmersnet.utils.FirebaseUtil;
 import com.example.farmersnet.utils.MyTimeUtil;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PostActivity extends AppCompatActivity {
 
@@ -32,12 +47,15 @@ public class PostActivity extends AppCompatActivity {
     private TextView articleTextView;
     private TextView dateTextView;
     private RecyclerView commentRecyclerView;
+    private ArrayList<Message> commentArrayList;
+    private CommentRecyclerAdapter commentRecyclerAdapter;
+    private String postId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
-        final String postId = getIntent().getExtras().getString("postId");
+        postId = getIntent().getExtras().getString("postId");
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Post");
@@ -47,11 +65,18 @@ public class PostActivity extends AppCompatActivity {
         postImageView = findViewById(R.id.post_rec_imageView);
         dateTextView = findViewById(R.id.post_rec_time_textView);
         commentRecyclerView = findViewById(R.id.comment_recyclerView);
+        commentArrayList = new ArrayList<Message>();
 
         FirebaseUtil.openFireBaseReference("Posts", this);
         firebaseFirestore = FirebaseUtil.firebaseFirestore;
         collectionReference = FirebaseUtil.collectionReference;
         postList = FirebaseUtil.postArrayList;
+
+        //Recycler adapter
+        commentRecyclerAdapter = new CommentRecyclerAdapter(commentArrayList);
+        commentRecyclerView.setLayoutManager(new LinearLayoutManager(PostActivity.this, LinearLayoutManager.VERTICAL, false));
+        commentRecyclerView.setAdapter(commentRecyclerAdapter);
+        getComments();
 
         collectionReference.document(postId).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
@@ -79,6 +104,52 @@ public class PostActivity extends AppCompatActivity {
                 }
             }
         });
+    }
 
+    private void getComments() {
+
+        collectionReference.document(postId).collection("messages").orderBy("timestamp").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                if (!queryDocumentSnapshots.isEmpty()) {
+                    for (@NonNull DocumentChange doc : queryDocumentSnapshots.getDocumentChanges()) {
+
+                        if(doc.getType()== DocumentChange.Type.ADDED){
+                            Message message = doc.getDocument().toObject(Message.class);
+                            commentArrayList.add(message);
+                            commentRecyclerAdapter.notifyDataSetChanged();
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    private void AddComment(String text, String user_id, String downloadUri){
+        final Map<String, Object> messageMap = new HashMap<>();
+        messageMap.put("text", text);
+        messageMap.put("timestamp", FieldValue.serverTimestamp());
+        messageMap.put("user_id", user_id);
+        if(downloadUri != null){
+            messageMap.put("image", downloadUri);
+        }
+
+//        sendMessageIcon.setEnabled(false);
+//        collectionReference
+//                .add(messageMap)
+//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//                    @Override
+//                    public void onSuccess(DocumentReference documentReference) {
+//                        messageEditText.setText("");
+//                        sendMessageIcon.setEnabled(true);
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        sendMessageIcon.setEnabled(true);
+//                        Toast.makeText(ChatActivity.this, "Error: "+ e, Toast.LENGTH_SHORT).show();
+//                    }
+//                });
     }
 }
